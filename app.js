3d2d6505864f22e810f520bd41a61b6b6ebd89da
clfhhc <clfhhc@gmail.com>
2018-05-09 12:27:04 -0700

    Complete refactoring to class and add states

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+let App = () =>(
+    <div>
+        <h2>My Grocery List</h2>
+        <GroceryList groceryList={['Carrot', 'Tofu']}/>
+    </div>
+);
+
+var GroceryList = (props) => (
+    <ul>
+        {props.groceryList.map((item) => <GroceryListItem key={item.toString()} item={item} />)}
+    </ul>
+)
+
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            crossed: false
+        }
+    }
+
+    onListItemClick(){
+        this.setState((prevState) =>({
+            crossed: !prevState.crossed
+        }));
+    }
+
+    render() {
+        let style = {
+            textDecoration: this.state.crossed ? 'line-through' : 'none'
+        }
+        return (<li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>);
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
